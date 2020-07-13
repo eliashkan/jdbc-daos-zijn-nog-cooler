3a0c71e79d3a88584954527ade6ea59f9c762786
Elias Khan <khan.elias@gmail.com>
2020-07-13 12:17:04 +0200

    override demonstation

--- src/main/java/be/vdab/dao/BeerDAO.java
+++ src/main/java/be/vdab/dao/BeerDAO.java
@@ -1,4 +1,10 @@
 package be.vdab.dao;
 
+import be.vdab.BeerException;
+import be.vdab.model.Beer;
+
 public interface BeerDAO {
+    Beer getBeerByID(int id) throws BeerException;
+
+    void updateBeer(Beer beer) throws BeerException;
 }
